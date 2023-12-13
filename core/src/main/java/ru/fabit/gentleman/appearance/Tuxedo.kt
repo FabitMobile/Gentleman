0799e692efac4baf491cbbe2da607e4b3e19ccbb
malec <andrey193728@yandex.ru>
2023-12-13 12:16:02 +0300

    open class Tuxedo

--- core/src/main/java/ru/fabit/gentleman/appearance/Tuxedo.kt
+++ core/src/main/java/ru/fabit/gentleman/appearance/Tuxedo.kt
@@ -2,7 +2,7 @@ package ru.fabit.gentleman.appearance
 
 import ru.fabit.gentleman.R
 
-class Tuxedo(
+open class Tuxedo(
     override val rationaleLayoutResId: Int = R.layout.tuxedo,
     override val settingLayoutResId: Int = R.layout.inner_chamber,
     override val appearanceClass: Class<out Appearance> = Appearance::class.java,
